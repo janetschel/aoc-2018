ac35d0e35b75751401aa357ffc5c08956225ee27
Jan Etschel <janetschel@iMac-von-Jan.local>
2021-08-26 14:29:16 +0200

    added .gitignore

--- src/main/java/dev/janetschel/Main.java
+++ src/main/java/dev/janetschel/Main.java
@@ -1,12 +1,13 @@
 package dev.janetschel;
 
 import dev.janetschel.calendar.day01.Day01;
+import dev.janetschel.calendar.day02.Day02;
 import dev.janetschel.utils.GenericDay;
 
 public class Main {
     public static void main(String[] args) {
         // Change L9 constructor call to change day output
-        GenericDay day = new Day01();
+        GenericDay day = new Day02();
 
         var solutionPart1 = day.solvePt1();
         var solutionPart2 = day.solvePt2();
---
+++ src/main/java/dev/janetschel/calendar/day02/Day02.java
@@ -0,0 +1,21 @@
+package dev.janetschel.calendar.day02;
+
+import dev.janetschel.utils.GenericDay;
+
+import java.util.ArrayList;
+
+import static dev.janetschel.utils.File.readFile;
+
+public class Day02 implements GenericDay {
+    private final ArrayList<String> input = readFile("02");
+
+    @Override
+    public String solvePt1() {
+        return null;
+    }
+
+    @Override
+    public String solvePt2() {
+        return null;
+    }
+}
