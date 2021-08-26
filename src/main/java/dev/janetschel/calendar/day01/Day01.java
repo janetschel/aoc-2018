7b238f271edd13e42aa140d90081ad353791f8b1
Jan Etschel <janetschel@iMac-von-Jan.local>
2021-08-26 14:18:16 +0200

    solve day 1

--- src/main/java/dev/janetschel/calendar/day01/Day01.java
+++ src/main/java/dev/janetschel/calendar/day01/Day01.java
@@ -3,6 +3,7 @@ package dev.janetschel.calendar.day01;
 import dev.janetschel.utils.GenericDay;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import static dev.janetschel.utils.File.readFile;
 
@@ -11,11 +12,32 @@ public class Day01 implements GenericDay {
 
     @Override
     public String solvePt1() {
-        return null;
+        return input.stream()
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum)
+                .toString();
     }
 
     @Override
     public String solvePt2() {
+        var arr = input.stream()
+                .map(Integer::parseInt)
+                .toList()
+                .toArray(Integer[]::new);
+
+        var seenFrequencies = new HashSet<Integer>();
+        var index = 0;
+        var sum = 0;
+
+        while (index >= 0) {
+            if (seenFrequencies.contains(sum)) {
+                return Integer.toString(sum);
+            }
+
+            seenFrequencies.add(sum);
+            sum += arr[index++ % arr.length];
+        }
+
         return null;
     }
 }
